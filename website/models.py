9b3ea4fe4fce67cbb414d5e8fa92d646934b3961
Ramya Govindaraj <ramyagovind13@gmail.com>
2023-10-30 16:53:29 -0700

    Initialized flask web app and configured PostgreSQL connection

--- website/models.py
+++ website/models.py
@@ -1,3 +1,10 @@
 '''
 Models for shop-easy app
 '''
+from . import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    role = db.Column(db.String(50))
