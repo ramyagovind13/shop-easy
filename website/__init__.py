cc3e5bdd09c4cfa063e4fd0039f66996770f2d0f
chethana613 <chetha@pdx.edu>
2023-10-31 16:32:01 -0700

    Auth setup

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_manager
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/shop_easy'
+    app.config['SECRET_KEY'] = 'secretkey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/shop_easy'
     db.init_app(app)
 
     from .views import views
