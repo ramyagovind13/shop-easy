cebed98f95c92330df2bf5cdaa37697dd21b5021
chethana613 <chetha@pdx.edu>
2023-10-31 16:25:00 -0700

    Added a column-Name to user table

--- website/models.py
+++ website/models.py
@@ -2,9 +2,11 @@
 Models for shop-easy app
 '''
 from . import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    role = db.Column(db.String(50))
+    role = db.Column(db.String(50)) 
+    name = db.Column(db.String(50))
