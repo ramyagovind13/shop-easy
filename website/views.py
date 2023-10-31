394e8339a4276b830a3f8df7a35c5508991b0973
Ramya Govindaraj <ramyagovind13@gmail.com>
2023-10-30 21:05:09 -0700

    Removed GET from home view

--- website/views.py
+++ website/views.py
@@ -5,6 +5,6 @@ Main views of shop-easy app
 from flask import Blueprint
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['GET'])
+@views.route('/')
 def home():
     return "<h1>Welcome to shop-easy app !!</h1>"
