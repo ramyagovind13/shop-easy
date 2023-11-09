9fa2fd37b5a40a678bb4468f4d65c9bfb136d152
Ramya Govindaraj <ramyagovind13@gmail.com>
2023-11-09 15:09:44 -0800

    Implemented GET inventory API to get all products details in the Inventory table

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User
+from . models import User
 from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
@@ -7,19 +7,20 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form.get('username')
+        email = request.form.get('email')
         password = request.form.get('password')
-
+        print(email, password)
         user = User.query.filter_by(email=email).first()
+        print(user)
         if user:
             if user.password == password :
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+                return redirect(url_for('views.get_inventory'))
             else:
-                print('Incorrect password, try again.', category='error')
+                flash('Incorrect password, try again.', category='error')
         else:
-            print('Sorry! Your email is not registered with Shop Easy.', category='error')
+            flash('Sorry! Your email is not registered with Shop Easy.', category='error')
 
     return render_template("login.html", user=current_user)
 
@@ -27,5 +28,4 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login'))
-
+    return redirect(url_for('views.home'))
--- website/views.py
+++ website/views.py
@@ -2,9 +2,23 @@
 Main views of shop-easy app
 '''
 
+import logging
 from flask import Blueprint, render_template
+from flask_login import login_required
+
+from student.inventory_details import get_inventory_details
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/get-inventory', methods=['GET'])
+@login_required
+def get_inventory():
+    try:
+        inventory_products = get_inventory_details()
+        print(inventory_products)
+        return render_template("get_inventory.html", products=inventory_products)
+    except Exception as e:
+        logging.exception(e)
