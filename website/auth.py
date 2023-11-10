d2e64f7560e076290906ee2ee6ea409b7a9a3b44
Mounisha Konduru <mkonduru@pdx.edu>
2023-11-09 18:36:19 -0800

    Modifeid admin code

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User
+from models import User
 from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
@@ -15,7 +15,7 @@ def login():
             if user.password == password :
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+                return redirect(url_for('views.admin'))
             else:
                 print('Incorrect password, try again.', category='error')
         else:
