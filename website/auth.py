e850a6e7073a6961580aa7b756bc2c452265aa11
Mounisha Konduru <mkonduru@pdx.edu>
2023-11-10 23:45:32 -0800

    Revert "Login Unit Test cases"
    This reverts commit 19863f49e535c2c48b04d8d75daf3e9bafe77034.

--- website/auth.py
+++ website/auth.py
@@ -1,25 +1,8 @@
-from models import User
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from flask_login import login_required, current_user, login_user, logout_user
-
-class AuthService:
-    @staticmethod
-    def login(email, password):
-        user = User.query.filter_by(email=email).first()
-        if user and user.password == password:
-            login_user(user, remember=True)
-            return True, "Logged in successfully!"
-        else:
-            return False, "Incorrect email or password."
-
-    @staticmethod
-    def logout():
-        logout_user()
-        return "Logged out successfully."
-
+from models import User
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
-auth_service = AuthService()
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -27,22 +10,16 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        success, message = auth_service.login(email, password)
-        if success:
-            flash(message, category='success')
-            return redirect(url_for('views.admin'))
-        print(email, password)
         user = User.query.filter_by(email=email).first()
-        print(user)
         if user:
             if user.password == password :
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.get_inventory'))
+                return redirect(url_for('views.admin'))
             else:
-                flash('Incorrect password, try again.', category='error')
+                print('Incorrect password, try again.', category='error')
         else:
-            flash('Sorry! Your email is not registered with Shop Easy.', category='error')
+            print('Sorry! Your email is not registered with Shop Easy.', category='error')
 
     return render_template("login.html", user=current_user)
 
@@ -50,4 +27,5 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('views.home'))
+    return redirect(url_for('auth.login'))
+
