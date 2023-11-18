c10f0a6f5e9f28a2cbb6a7b5db708cd704335345
Adarsh <ak5439752@gmail.com>
2023-11-18 12:00:00 -0800

    email confirm

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager #flask_login creates connection between the cookie and actual data in the DB
+from flask import Flask
+from flask_mail import Mail
+
+app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'your_email@example.com'
+app.config['MAIL_PASSWORD'] = 'your_email_password'
+app.config['MAIL_DEFAULT_SENDER'] = 'your_email@example.com'
+
+mail = Mail(app)
+
+
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secretkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/shop_easy'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Adarsh123#@localhost:5432/shop_easy'
     db.init_app(app)
 
     login_manager = LoginManager()
