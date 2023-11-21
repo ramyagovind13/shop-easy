82901ee1c77573b36f121f44fff9357ac1e1555f
Adarsh <ak5439752@gmail.com>
2023-11-20 16:34:43 -0800

    made required changes

--- website/__init__.py
+++ website/__init__.py
@@ -1,32 +1,31 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager #flask_login creates connection between the cookie and actual data in the DB
-from flask import Flask
+from flask_login import LoginManager
 from flask_mail import Mail
 
-app = Flask(__name__)
-
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'shopeasy135@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Shopeasy0909'
-app.config['MAIL_DEFAULT_SENDER'] = 'shopeasy135@gmail.com'
-
-mail = Mail(app)
 db = SQLAlchemy()
+mail = Mail()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secretkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Adarsh123#@localhost:5432/shop_easy'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/shop_easy'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable Flask-SQLAlchemy modification tracking
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = False
+    app.config['MAIL_USERNAME'] = 'shopeasy135@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'Shopeasy0909'
+    app.config['MAIL_DEFAULT_SENDER'] = 'shopeasy135@gmail.com'
+
     db.init_app(app)
+    mail.init_app(app)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-        
+
     from .views import views
     from .auth import auth
 
@@ -38,7 +37,7 @@ def create_app():
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
-         
+
     with app.app_context():
         db.create_all()
 
--- website/views.py
+++ website/views.py
@@ -1,16 +1,9 @@
-'''
-Main views of shop-easy app
-'''
-
+from flask_mail import Message
+from datetime import datetime
 import logging
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
-from datetime import datetime
-
-from student.inventory_details import get_inventory_details
-from student.cart import add_cart
-from admin.inventory import add
-from student.cart import get_cart_details, get_user_inventory_details
+from . import mail  # Import the mail instance from the package
 
 views = Blueprint('views', __name__)
 
@@ -18,75 +11,32 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html")
 
-@views.route('/get-inventory', methods=['GET'])
+# ... (your existing routes)
+
+@views.route('/place-order', methods=['POST'])
 @login_required
-def get_inventory():
+def place_order():
     try:
-        inventory_products = get_inventory_details()
-        if inventory_products:
-            unique_categories = set(product.category for product in inventory_products)
-            return render_template("get_inventory.html", products=inventory_products,
-                                   categories=unique_categories)
-        else:
-            return render_template("get_inventory.html", products=[],
-                                   categories=[])
+        # ... (existing code to handle order placement)
+
+        # Send email notification to the user
+        send_order_confirmation_email(current_user.email)
+
+        flash('Order placed successfully! Check your email for confirmation.', category='success')
+        return redirect(url_for('views.home'))
+
     except Exception as e:
         logging.exception(e)
+        flash('An unexpected error occurred. Please try again later.', category='error')
+        return render_template("place_order.html", user=current_user)
 
-@views.route('admin/add-inventory', methods=['POST'])
-@login_required
-def add_inventory():
-    name = request.form.get("name")
-    description = request.form.get("description")
-    quantity = int(request.form.get("quantity"))
-    category = request.form.get("category")
-    weight = int(request.form.get("weight"))
-    expiry_date = datetime.strptime(request.form.get("expiry_date"), '%m-%d-%Y')
-    print(f"Name: {name}, Description: {description}, Quantity: {quantity}, Category: {category}, Weight: {weight}, Expiry Date: {expiry_date}")
-    # Validating the null check of input fields
-    if any(value is None or value == "" for value in (name, description, quantity, category, weight, expiry_date)):
-        flash("One or more form values are missing", category='error')
-        return render_template("login.html", user=current_user)   
-    else:
-        status = add(name, description, quantity, category, weight, expiry_date)
-        return redirect(url_for('views.get_inventory')) if status else render_template("login.html", user=current_user) #Redirect the else part to add_inventory html page 
-    
-
-@views.route('student/add-to-cart', methods=['POST'])
-@login_required
-def add_to_cart():
-    data = request.get_json()
-    product_id = data.get('productId')
-    quantity = data.get('quantity')
-    status = add_cart(product_id,quantity)  
-    if status:
-        return jsonify({'message': 'Item added to cart successfully'}), 200
-    else:
-        return jsonify({'message': 'Sorry! Unexpected error occured while adding the item to the cart'}), 500
-
-
-@views.route('/cart', methods=['GET'])
-@login_required
-def get_cart():
+def send_order_confirmation_email(email):
     try:
-        cart_details = get_cart_details(current_user)
-        inventory_details = get_user_inventory_details(current_user)
-        mapping_dict = {product.sku: product for product in cart_details}
-        cart_products = [
-            {"sku": product_details.sku,
-            "name": product_details.name,
-            "quantity": mapping_dict[product_details.sku].quantity}
-            for product_details in inventory_details
-            if product_details.sku in mapping_dict
-        ]
-        total_quantity = sum(product['quantity'] for product in cart_products)
-        if cart_products:
-            return render_template("checkout.html", cart_details=cart_products,
-                                   total_quantity=total_quantity)
-        else:
-            flash("Cart is Empty !!", category='error')
-            return render_template("checkout.html", cart_details=[],
-                                   total_quantity=0)
+        msg = Message('Order Confirmation', recipients=[email])
+        msg.body = 'Thank you for placing an order with Shop Easy. Your order has been successfully placed.'
+        mail.send(msg)
+        return True
+
     except Exception as e:
         logging.exception(e)
-
+        return False
